fa3dc6d616d8aa495a296ebea556d95d9959e094
anabelyong <banana180501@gmail.com>
2024-06-08 15:16:19 +0100

    ensure that PORT environment variable is used

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import random
 
@@ -40,5 +41,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
